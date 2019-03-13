7f105167f2a12064ac4bafe901b7d13f82e14eed
FX-Wood <fxwood2@gmail.com>
2019-03-13 15:01:23 -0700

    fixed case in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 const toDos = ["Buy ice cream", "eat ice cream", "delete the gym"]
 
-ReactDOM.render(<ToDo todos={toDos}/>, document.getElementById('root'));
+ReactDOM.render(<ToDo toDos={toDos}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
