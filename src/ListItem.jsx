5779bce7322c8c99763f02c1787c50a86ba9eb81
FX-Wood <fxwood2@gmail.com>
2019-03-13 14:43:24 -0700

    Created list item component

---
+++ src/ListItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+    render() {
+        return (
+            <li className="ListItem">List Item</li>
+        )
+    }
+}
+
+export default ListItem;
