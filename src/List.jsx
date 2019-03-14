d0c59490a032ad9988695be0286bfe4c70446f9b
FX-Wood <fxwood2@gmail.com>
2019-03-13 18:42:01 -0700

    Implemented state and clear button

--- src/List.jsx
+++ src/List.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import ListItem from './ListItem'
 class List extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            toDos: props.toDos
+        }
+        this.clearList = this.clearList.bind(this)
+    }
+    clearList(e) {
+        console.log('clearing list!')
+        this.setState({
+            toDos: []
+        })
+    }
+
+    addToList(e) {
+        this.state.toDos.push('This is a new item')
+    }
+
     render() {
-        const items = this.props.toDos.map((toDo, i) => <ListItem toDo={toDo} key={i}></ListItem>)
+        const items = this.state.toDos.map((toDo, i) => <ListItem toDo={toDo} key={i}></ListItem>)
         return (
-            <ul className="List">
-                {items}
-            </ul>
+            <>
+                <ul className="List">
+                    {items}
+                </ul>
+                <button onClick={this.clearList}>Clear List</button>
+            </>
         )
     }
 }
